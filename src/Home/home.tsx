eeea714fb297d51911ad8170da30c8ff921fd09d
drunk喵咪 <1684838553@qq.com>
2022-04-13 15:03:47 +0800

    Create home.tsx

---
+++ src/Home/home.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './home.module.css'
+
+export default function Home(): JSX.Element {
+    return <div className={styles.title}>
+        dddd <span className={styles.bar}>hhh</span>
+    </div>
+}
