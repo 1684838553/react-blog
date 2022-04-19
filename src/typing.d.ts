1c387a1685b0e29ea9370d61fb4a40cdd517399a
drunk喵咪 <1684838553@qq.com>
2022-04-19 09:15:52 +0800

    Update typing.d.ts

--- src/typing.d.ts
+++ src/typing.d.ts
@@ -3,3 +3,10 @@ declare module '*.module.css' {
     const classes: { [key: string]: string };
     export default classes;
 }
+
+declare module 'react-router-dom'
+declare module 'showdown'
+
+declare module '*.jpeg'
+declare module '*.jpg'
+declare module '*.png'
