d600b0f523ad1fc7903817451cae98f7c7342295
drunk喵咪 <1684838553@qq.com>
2022-04-19 09:15:28 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Header from './Component/Header/header'
 import Home from './Home/home'
+import Login from './Login/login'
+import Write from './Write/write'
+import Article from './Article/article'
 
 function App() {
+  // const currentUser = true;
+  // const pathForNoHeader = ['/login']
+  // const pathname = window.location.pathname
+
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/"><Home /> </Route>
+        <Route path="/write"> <Write /> </Route>
+        <Route path="/register"> <Login /> </Route>
+        <Route path="/login"> <Login /></Route>
+        <Route path="/article"> <Article /></Route>
+      </Switch>
+    </Router>
   );
 }
 
